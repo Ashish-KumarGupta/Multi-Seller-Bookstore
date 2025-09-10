76ceed076bf5a6fe8eaba0a5deded96eb1a83b3f
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2025-09-10 14:45:49 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: "mysql",
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
     logging: false,
   }
 );
