9253e205449ca5f4fa4295ca8913e08ab0f1178b
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2025-09-12 18:57:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const sequelize = require("./config/database"); // import sequelize instance
+const sequelize = require("./config/db");
 
 const app = express();
 
