8f0882bf05ed5c53e8f227e5792488570f674f69
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2025-09-12 19:02:23 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,21 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-  logging: false,
-});
+  }
+);
 
 module.exports = sequelize;
-
-
